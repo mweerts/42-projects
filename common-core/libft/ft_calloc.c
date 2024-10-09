584209b2706c7d75cb6c2f797f6fcd9fdb85d9bc
mweerts <53750233+mweerts@users.noreply.github.com>
2024-10-10 00:45:48 +0200

    all basic function

---
+++ common-core/libft/ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maxweert <maxweert@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/09 23:40:09 by maxweert          #+#    #+#             */
+/*   Updated: 2024/10/10 00:35:17 by maxweert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	unsigned char	*ret;
+
+	ret = malloc(count * size);
+	if (!ret)
+		return (0);
+	ft_bzero(ret, count * size);
+	return ((void *)ret);
+}
