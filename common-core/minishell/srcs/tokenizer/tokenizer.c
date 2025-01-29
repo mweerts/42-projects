f6e43bc35dcba80b56ff54e75167735aedde9369
llebugle <llebugle@student.s19.be>
2025-01-29 20:24:59 +0100

    added some tests and refactored parts of the tokenizer

--- common-core/minishell/srcs/tokenizer/tokenizer.c
+++ common-core/minishell/srcs/tokenizer/tokenizer.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/minishell.h"
+#include "minishell.h"
 
 static inline int	is_operator(char c)
 {
