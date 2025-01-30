ee4fc0ec306ba2eb33ad2ab4d1cc3d77f9936ff6
maxweert <maxweert@student.s19.be>
2025-01-31 00:00:11 +0100

    factoring function

--- common-core/minishell/srcs/parsing/tree.c
+++ common-core/minishell/srcs/parsing/tree.c
@@ -6,7 +6,7 @@
 /*   By: maxweert <maxweert@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 20:31:11 by maxweert          #+#    #+#             */
-/*   Updated: 2025/01/30 23:32:46 by maxweert         ###   ########.fr       */
+/*   Updated: 2025/01/30 23:59:13 by maxweert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,26 +38,28 @@ static t_tree_node	*new_node(t_node_type type)
 	return (node);
 }
 
+static t_node_type	get_node_type(t_token_type token_type)
+{
+	if (token_type == TOKEN_AND)
+		return (NODE_AND);
+	if (token_type == TOKEN_OR)
+		return (NODE_OR);
+	if (token_type == TOKEN_PIPE)
+		return (NODE_PIPE);
+	return (NODE_COMMAND);
+}
+
 t_tree_node	*new_tree(t_data *data, t_token **token)
 {
 	t_tree_node	*root;
 	t_tree_node	*tmp_node;
-	t_command	*command;
-	t_token		*token_head;
 
-	token_head = *token;
 	root = NULL;
 	tmp_node = NULL;
 	if (!(*token))
 		return (NULL);
 	while ((*token))
 	{
-		if ((*token)->type == TOKEN_AND)
-			tmp_node = new_node(NODE_AND);
-		else if ((*token)->type == TOKEN_OR)
-			tmp_node = new_node(NODE_OR);
-		else if ((*token)->type == TOKEN_PIPE)
-			tmp_node = new_node(NODE_PIPE);
 		if ((*token)->type == TOKEN_OPEN_PAR)
 		{
 			*token = (*token)->next;
@@ -65,14 +67,10 @@ t_tree_node	*new_tree(t_data *data, t_token **token)
 		}
 		else if ((*token)->type == TOKEN_CLOSE_PAR)
 			return (root);
-		else if ((*token)->type == TOKEN_WORD)
-		{
-			tmp_node = new_node(NODE_COMMAND);
+		else
+			tmp_node = new_node(get_node_type((*token)->type));
+		if (tmp_node->type == NODE_COMMAND)
 			tmp_node->cmd = get_command(token);
-			while ((*token) && ((*token)->type != TOKEN_AND && (*token)->type != TOKEN_OR
-				&& (*token)->type != TOKEN_PIPE && (*token)->type != TOKEN_OPEN_PAR && (*token)->type != TOKEN_CLOSE_PAR))
-				(*token) = (*token)->next;
-		}
 		if (!root)
 			root = tmp_node;
 		else if (root->left && !root->right)
