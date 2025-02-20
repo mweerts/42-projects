c4b9a1636b3f6315b9f54bd408b39d0c28eb5779
llebugle <llebugle@student.s19.be>
2025-02-20 15:29:26 +0100

    adding redirections

--- common-core/minishell/srcs/exec/redirect.c
+++ common-core/minishell/srcs/exec/redirect.c
@@ -12,11 +12,18 @@
 
 #include "minishell.h"
 
-int	process_outfile(t_data *data, char *filename)
+#define APPEND 1
+#define TRUNC 2
+
+int	process_outfile(t_data *data, char *filename, int type)
 {
 	int	fd;
+	int	flags;
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	flags = O_WRONLY | O_CREAT | O_TRUNC;
+	if (type == APPEND)
+		flags = O_WRONLY | O_CREAT | O_APPEND;
+	fd = open(filename, flags, 0644);
 	if (fd == -1)
 	{
 		ft_printf_fd(STDERR_FILENO, "minishell: %s: %s\n", filename,
@@ -31,6 +38,25 @@ int	process_outfile(t_data *data, char *filename)
 	return (SUCCESS);
 }
 
+int	process_infile(t_data *data, char *filename)
+{
+	int	fd;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		ft_printf_fd(STDERR_FILENO, "minishell: %s: %s\n", filename,
+			strerror(errno));
+		return (ERROR);
+	}
+	if (dup2(fd, STDIN_FILENO) == -1)
+	{
+		close(fd);
+		return (ERROR);
+	}
+	return (SUCCESS);
+}
+
 int	redirect_fd(t_data *data, t_command *cmd)
 {
 	t_redirection	*curr;
@@ -38,15 +64,21 @@ int	redirect_fd(t_data *data, t_command *cmd)
 	curr = cmd->redirections;
 	while (curr)
 	{
-		if (curr->type == TOKEN_OUT)
+		if (curr->type == TOKEN_IN)
 		{
-			if (process_outfile(data, curr->filename) == ERROR)
+			if (process_infile(data, curr->filename) == ERROR)
 				return (data->exit_code = errno, ERROR);
 		}
-		// else if (curr->type == TOKEN_IN)
-		// 	process_infile();
-		// else if (curr->type == TOKEN_APPEND)
-		// 	process_append();
+		else if (curr->type == TOKEN_OUT)
+		{
+			if (process_outfile(data, curr->filename, TRUNC) == ERROR)
+				return (data->exit_code = errno, ERROR);
+		}
+		else if (curr->type == TOKEN_APPEND)
+		{
+			if (process_outfile(data, curr->filename, APPEND) == ERROR)
+				return (data->exit_code = errno, ERROR);
+		}
 		else
 			return (ERROR);
 		curr = curr->next;
