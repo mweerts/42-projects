c07ac16d491e423cdd74f01cceaa3cb2fa8aee8e
mweerts <maxweert@student.s19.be>
2025-04-02 18:44:52 +0200

    norme

--- common-core/cub3d/srcs/game/portal.c
+++ common-core/cub3d/srcs/game/portal.c
@@ -54,7 +54,7 @@ static int	is_destination_submap_ok(t_data *data, t_raycasting *ray,
 		t_map_element dir, int **cpy)
 {
 	if (dir == NORTH && !flood_fill(data->map, cpy, ray->ray_y + 1, ray->ray_x))
-		return ( free_matrix(cpy, data->map->height), 1);
+		return (free_matrix(cpy, data->map->height), 1);
 	else if (dir == SOUTH && !flood_fill(data->map, cpy, ray->ray_y - 1,
 			ray->ray_x))
 		return (free_matrix(cpy, data->map->height), 1);
@@ -77,9 +77,7 @@ static int	is_portal_possible(t_data *data, t_raycasting *ray, int button)
 	if (data->map->matrix[ray->ray_y][ray->ray_x] != 1)
 		return (0);
 	if (!is_portal_pos_free(data, ray, dir))
-		return (printf("PAS OK\n"), 0);
-	if (dir == WEST)
-		printf("%d %d\n", ray->ray_x, ray->ray_y);
+		return (0);
 	cpy = (int **)ft_calloc((data->map->height + 1), sizeof(int *));
 	if (!cpy)
 		return (print_err(MSG_ERR_MALLOC), -1);
