b85d1795cb1baef3108da09d1c5de72e7bdc86cc
Lucas Lebugle <lu.lebugle@gmail.com>
2025-08-23 22:06:37 +0200

    fix: protected function using stat against unintialized value

--- common-core/webserv/srcs/lib/file_utils.cpp
+++ common-core/webserv/srcs/lib/file_utils.cpp
@@ -24,31 +24,41 @@ bool pathExist(const std::string& path) {
 
 bool isFile(const std::string& path) {
     struct stat fileInfo;
-    stat(path.c_str(), &fileInfo);
+    if (stat(path.c_str(), &fileInfo) != 0) {
+        return false;
+    }
     return S_ISREG(fileInfo.st_mode);
 }
 
 bool isDirectory(const std::string& path) {
     struct stat fileInfo;
-    stat(path.c_str(), &fileInfo);
+    if (stat(path.c_str(), &fileInfo) != 0) {
+        return false;
+    }
     return S_ISDIR(fileInfo.st_mode);
 }
 
 bool isReadable(const std::string& path) {
     struct stat fileInfo;
-    stat(path.c_str(), &fileInfo);
+    if (stat(path.c_str(), &fileInfo) != 0) {
+        return false;
+    }
     return S_IRUSR & fileInfo.st_mode && !access(path.c_str(), R_OK);
 }
 
 bool isWritable(const std::string& path) {
     struct stat fileInfo;
-    stat(path.c_str(), &fileInfo);
+    if (stat(path.c_str(), &fileInfo) != 0) {
+        return false;
+    }
     return S_IWUSR & fileInfo.st_mode && !access(path.c_str(), W_OK);
 }
 
 bool isExecutable(const std::string& path) {
     struct stat fileInfo;
-    stat(path.c_str(), &fileInfo);
+    if (stat(path.c_str(), &fileInfo) != 0) {
+        return false;
+    }
     return (fileInfo.st_mode & S_IXUSR) != 0;
 }
 
