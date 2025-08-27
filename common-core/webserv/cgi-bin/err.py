6d877234898f6adf46195b502faeab8abae3b843
Lucas Lebugle <lu.lebugle@gmail.com>
2025-08-27 19:33:50 +0200

    added err script to test

---
+++ common-core/webserv/cgi-bin/err.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import time
+import sys
+import ctypes
+
+ctypes.string_at(0)
+
+# Intestazioni CGI
+print("Content-Type: text/plain\n")
+
+# Simuliamo output lento e abbondante
+for i in range(1, 501):  # 500 righe
+    print(f"Linea {i}: Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+    sys.stdout.flush()  # forza l’invio al client
+    time.sleep(0.05)    # 50ms di ritardo per simulare lentezza
+
