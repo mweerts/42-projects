81de0cd8544097c05d072d25c301da537c3b038f
Lucas Lebugle <lu.lebugle@gmail.com>
2025-11-20 08:37:56 +0100

    some things works, some don't..

--- common-core/ft_transcendence/services/frontend/srcs/pages/home.tsx
+++ common-core/ft_transcendence/services/frontend/srcs/pages/home.tsx
@@ -1,17 +1,18 @@
+import { ButtonLink } from "@/components/button";
 import { MainLayout } from "@/components/main-layout";
 
 const Home = () => {
 //   console.log(import.meta.env.DEV);
   return (
     <MainLayout>
+        <div className="flex flex-col items-center justify-center min-h-[80vh] px-6">
         <h1 className="text-3xl font-bold">Welcome to Transcendence</h1>
         <p className="text-gray-600 mt-2">Your 3D Pong game awaits!</p>
-        <a
-          href="https://local.drizzle.studio"
-          className="text-blue-500 hover:text-blue-700"
-        >
-          Drizzle Studio
-        </a>
+		<div className="flex flex-row gap-4 py-8">
+			<ButtonLink to="/auth/login" variant="default" size="lg">Login</ButtonLink>
+			<ButtonLink to="/auth/signup" variant="default" size="lg">Signup</ButtonLink>
+		</div>
+        </div>
     </MainLayout>	
   );
 };
